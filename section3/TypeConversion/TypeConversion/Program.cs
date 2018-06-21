fc51c4a33628e8dd52f895f9bf5b996b64a25051
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-21 19:25:48 -0400

    Convert.ToInt32()

--- section3/TypeConversion/TypeConversion/Program.cs
+++ section3/TypeConversion/TypeConversion/Program.cs
@@ -7,6 +7,9 @@ namespace TypeConversion
         static void Main(string[] args)
         {
             var number = "1234";
+            int i = Convert.ToInt32(number);
+
+            Console.WriteLine(i);
         }
     }
 }
