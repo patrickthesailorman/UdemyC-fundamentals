3d4e6f851999616ad772a4313a70b496e24ebd63
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-21 19:55:09 -0400

    divide float

--- section3/Operators/Operators/Program.cs
+++ section3/Operators/Operators/Program.cs
@@ -8,7 +8,7 @@ namespace Operators
         {
             var a = 10;
             var b = 3;
-            Console.WriteLine(a / b);
+            Console.WriteLine((float)a / (float)b);
         }
     }
 }
