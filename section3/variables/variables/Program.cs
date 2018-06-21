0c5d158f85b2ccc56788033cf58c968101d7a293
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-20 21:28:42 -0400

    const float Pi

--- section3/variables/variables/Program.cs
+++ section3/variables/variables/Program.cs
@@ -6,8 +6,7 @@ namespace variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14;
 
         }
     }
