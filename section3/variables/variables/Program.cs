5a06e787096ff5b478eba909d768722fe9a77c85
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-20 21:00:18 -0400

    First commit

---
+++ section3/variables/variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            Console.WriteLine(number);
+        }
+    }
+}
