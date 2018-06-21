0f603c989482d6d9735786dfa89980d0c0a04299
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-21 19:19:36 -0400

    int i = 1000;

--- section3/TypeConversion/TypeConversion/Program.cs
+++ section3/TypeConversion/TypeConversion/Program.cs
@@ -6,10 +6,10 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
-            int i = 1;
+            int i = 1000;
             byte b = (byte) i;
 
-            Console.WriteLine(i);
+            Console.WriteLine(b);
         }
     }
 }
