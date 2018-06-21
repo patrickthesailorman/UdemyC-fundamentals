281ea0d0d0c0020a60eb850a58e46bb597835408
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-21 19:58:21 -0400

    math

--- section3/Operators/Operators/Program.cs
+++ section3/Operators/Operators/Program.cs
@@ -9,7 +9,7 @@ namespace Operators
             var a = 1;
             var b = 2;
             var c = 3;
-            Console.WriteLine(a + b * c);
+            Console.WriteLine((a + b )* c);
         }
     }
 }
