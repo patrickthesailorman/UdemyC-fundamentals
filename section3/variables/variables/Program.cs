4527df22cd1d636cfed0d4ea7d7bea65aa61ddf6
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-20 21:28:03 -0400

    store float

--- section3/variables/variables/Program.cs
+++ section3/variables/variables/Program.cs
@@ -7,6 +7,7 @@ namespace variables
         static void Main(string[] args)
         {
             Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
 
         }
     }
