5fd67d76d5ea7e02b7b627239316f0739d521091
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-21 20:32:01 -0400

    classes

---
+++ section3/Classes/Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person 
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.firstName = "John";
+            john.lastName = "Smith";
+
+            john.Introduce();
+        }
+    }
+}
