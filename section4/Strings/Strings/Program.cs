f142a6948bd9dbdacb9375f8c26ba2056d107d6b
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-25 19:41:31 -0400

    Add strings

---
+++ section4/Strings/Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Mosh";
+
+            Console.WriteLine(FirstName);
+        }
+    }
+}
