d0e08524700e097ba98178ad3c761b99ea699bdf
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-25 23:41:36 -0400

    add increment

---
+++ section4/ValueTypes/ValueTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ValueTypes
+{
+    public class Person
+    {
+        public int Age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+        }
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+
+
+    }
+}
