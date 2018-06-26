7c50af6f8ad402f7221c44015729f699f988ca64
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 18:25:07 -0400

    Add switch

--- section5/Conditionals/Conditionals/Program.cs
+++ section5/Conditionals/Conditionals/Program.cs
@@ -6,7 +6,20 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-           
+            var season = Season.Autumn;
+             
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's Autumn and a beautiful season.");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's summer and a beautiful season.");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand that season.");
+                    break;
+            }
         }
     }
 }
