e58b7686c6c432f6fc94e6a74d017ef7785d0b94
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-25 20:13:10 -0400

    Add enums

---
+++ section4/Enums/Enums/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+        }
+    }
+}
