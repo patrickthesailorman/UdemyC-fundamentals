8e9c4542ea6e392f909e711b8568a64c47799028
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 19:39:39 -0400

    Add countdown

--- section5/Iterations/Iterations/Program.cs
+++ section5/Iterations/Iterations/Program.cs
@@ -6,7 +6,15 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i < 10; i++ )
+            for (var i = 1; i <= 10; i++ )
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var i = 10; i >= 1; i--)
             {
                 if (i % 2 == 0)
                 {
