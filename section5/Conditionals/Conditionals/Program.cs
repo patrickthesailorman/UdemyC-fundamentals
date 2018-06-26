b00d552bf295f0f820abf3d50890e3d8919b32ca
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 16:51:04 -0400

    Add else if...

--- section5/Conditionals/Conditionals/Program.cs
+++ section5/Conditionals/Conditionals/Program.cs
@@ -8,9 +8,13 @@ namespace Conditionals
         {
             int hour = 10;
 
-            if(hour > 0 && hour < 12)
+            if (hour > 0 && hour < 12)
 
-            Console.WriteLine("It's morning");
+                Console.WriteLine("It's morning");
+
+            else if (hour >= 12 && hour < 18)
+
+                Console.WriteLine("It's afternoon.");
         }
     }
 }
