f9494779a78b54c151cd5f3136de8f64954139de
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 19:50:39 -0400

    foreach number

--- section5/Iterations/Iterations/Program.cs
+++ section5/Iterations/Iterations/Program.cs
@@ -6,14 +6,11 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            var name = "John Smith";
-            //for (var i = 0; i < name.Length; i++) 
-            //{
-            //    Console.WriteLine(name[i]);
-            //}
-            foreach (var character in name)
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach(var number in numbers)
             {
-                Console.WriteLine(character);
+                Console.WriteLine(number);
             }
 
         }
