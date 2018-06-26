273ea49033274244b8653e2fdcbaa7da7917b78a
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-25 23:21:10 -0400

    Add ReferenceType

---
+++ section4/Reference_Types/Reference_Types/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Reference_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+        }
+    }
+}
