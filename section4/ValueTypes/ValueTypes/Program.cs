bcd21020c91ede2291770f043a560f41c1d7a51f
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-25 23:45:28 -0400

    add person

--- section4/ValueTypes/ValueTypes/Program.cs
+++ section4/ValueTypes/ValueTypes/Program.cs
@@ -13,6 +13,10 @@ namespace ValueTypes
             var number = 1;
             Increment(number);
             Console.WriteLine(number);
+
+            var person = new Person() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
         }
         public static void Increment(int number)
         {
