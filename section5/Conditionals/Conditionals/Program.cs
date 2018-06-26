c913d0a8fa0c990cfabe6796096cb792d6acb253
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 18:13:25 -0400

    Add Seasons.cs

--- section5/Conditionals/Conditionals/Program.cs
+++ section5/Conditionals/Conditionals/Program.cs
@@ -6,17 +6,7 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            bool isGoldCustomer = true;
-
-            //float price;
-
-            //if (isGoldCustomer)
-            //    price = 19.95f;
-            //else
-            //price = 29.95f;
-
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
-            Console.WriteLine(price);
+           
         }
     }
 }
