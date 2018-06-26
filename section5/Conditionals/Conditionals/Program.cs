2c66625bdcf370be66263a5f660edc6bd3fba2c0
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 16:48:40 -0400

    Add if...

---
+++ section5/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if(hour > 0 && hour < 12)
+
+            Console.WriteLine("It's morning");
+        }
+    }
+}
