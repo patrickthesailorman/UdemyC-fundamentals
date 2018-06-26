81d84bc920ec6b7c2a61aa74b050ec919a0c4d94
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 17:59:03 -0400

    Add price

--- section5/Conditionals/Conditionals/Program.cs
+++ section5/Conditionals/Conditionals/Program.cs
@@ -6,18 +6,14 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            int hour = 10;
+            bool isGoldCustomer = true;
 
-            if (hour > 0 && hour < 12)
-            {
-                Console.WriteLine("It's morning");
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-                Console.WriteLine("It's afternoon.");
-            }
+            float price;
+
+            if (isGoldCustomer)
+                price = 19.95f;
             else
-                Console.WriteLine("It's Evening.");
+                price = 29.95f;
         }
     }
 }
