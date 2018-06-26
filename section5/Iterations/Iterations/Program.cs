18ccf338dccbdb38afb2169b603ce0de5d745543
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 19:45:50 -0400

    for loop

--- section5/Iterations/Iterations/Program.cs
+++ section5/Iterations/Iterations/Program.cs
@@ -6,20 +6,10 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i <= 10; i++ )
+            var name = "John Smith";
+            for (var i = 0; i < name.Length; i++) 
             {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-
-            for (var i = 10; i >= 1; i--)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(name[i]);
             }
 
         }
