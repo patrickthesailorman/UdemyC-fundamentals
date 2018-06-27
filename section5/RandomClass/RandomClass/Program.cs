2a9d11e5b154249863d3e97edc7e19e27186aded
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 20:31:11 -0400

    Add ascii fix

--- section5/RandomClass/RandomClass/Program.cs
+++ section5/RandomClass/RandomClass/Program.cs
@@ -9,7 +9,7 @@ namespace RandomClass
             var random = new Random();
             for (var i = 0; i < 10; i++)
             {
-                Console.WriteLine((char)'a' + random.Next(1,26));
+                Console.WriteLine((char)('a' + random.Next(0, 26)));
                 Console.WriteLine();
             }
            
