e3065d836ee1dadf5a6561c21835ebbda54a0203
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-27 18:36:41 -0400

    Add max

---
+++ section5/Excercises/MaxNum/MaxNum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MaxNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            var first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number: ");
+            var second = Convert.ToInt32(Console.ReadLine());
+
+            var max = (first > second) ? first : second;
+                Console.WriteLine(max);
+        }
+    }
+}
