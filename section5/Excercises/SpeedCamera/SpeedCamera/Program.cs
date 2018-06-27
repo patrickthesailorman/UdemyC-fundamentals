48a58e6b6aa5f43fe866a2b7aba05d26930dfb10
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-27 19:34:31 -0400

    Add speedCamera

---
+++ section5/Excercises/SpeedCamera/SpeedCamera/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SpeedCamera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demerits = 0;
+
+            Console.WriteLine("Enter the speed limit: ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter current speed: ");
+            var currentSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if(currentSpeed < speedLimit) 
+            {
+                Console.WriteLine("ok");
+            }
+            else if (currentSpeed > speedLimit)
+            {
+                demerits = (currentSpeed - speedLimit) / 5;
+                Console.WriteLine("You have: " + demerits + " demerits!");
+            }
+            if (demerits > 12)
+            {
+                Console.WriteLine("License Suspended!");
+            }
+        }
+    }
+}
