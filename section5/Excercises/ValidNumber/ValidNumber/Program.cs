a5c89e11b868e84292e8b2ca351a1f5446fd53a5
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-26 20:57:31 -0400

    Add excercises

---
+++ section5/Excercises/ValidNumber/ValidNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ValidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive number between 1 and 10: ");
+            var input = Convert.ToDouble(Console.ReadLine());
+
+            var check = (input >= 1 && input <= 10) ? "Valid" : "Invalid";
+
+            Console.WriteLine(check);
+        }
+    }
+}
