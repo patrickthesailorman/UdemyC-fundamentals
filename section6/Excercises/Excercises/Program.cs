e3be8dc684d75081afcaa295eafc52725a3a8e10
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-28 15:57:16 -0400

    Add Excercise

---
+++ section6/Excercises/Excercises/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name: ");
+            var name = Console.ReadLine();
+        }
+    }
+}
