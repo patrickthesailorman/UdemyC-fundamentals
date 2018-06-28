487d27a65f9ffd9a2520856e4e1cb7b4425f5a79
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-28 16:37:53 -0400

    Add Facebook WIP git push

---
+++ section6/Excercises/Facebook/Facebook/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Facebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = new String;
+
+            while (name != " ")
+            {
+                var names[] = new[];
+                Console.WriteLine("Enter name: ");
+                var name = Console.ReadLine();
+
+                names.push(name);
+            }
+            if (names.Count() == 1)
+            {
+                Console.WriteLine(names[0] + " likes this");
+            }
+            else if (names.Count() == 2)
+            {
+                Console.WriteLine(names[0] + " & " + names[1] + " like this");
+            }
+            else if (names.Count() > 2)
+            {
+                Console.WriteLine(names[0] + " , " + names[1] + " & {0} others like this", names.Count() - 2);
+            }
+        }
+    }
+}
