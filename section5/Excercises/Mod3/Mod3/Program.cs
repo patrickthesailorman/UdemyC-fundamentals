f75556d9d99f2810e2e0c7d5336e187f102014de
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-27 20:11:39 -0400

    Add count

---
+++ section5/Excercises/Mod3/Mod3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+
+            for (var i = 0; i < 100; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    count++; 
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
