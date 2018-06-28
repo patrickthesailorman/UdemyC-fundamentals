12e08910e3e5631d22830d72c6cd8bf84af2bed8
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-27 23:09:54 -0400

    Add Max

---
+++ section5/Excercises/FindMax/FindMax/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FindMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a series of numbers separated by commas: ");
+            var array = Console.ReadLine();
+            var nums = array.Split(",");
+            var max = Convert.ToInt32(nums[0]);
+
+            foreach (var num in nums)
+            {
+                var number = Convert.ToInt32(num);
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
