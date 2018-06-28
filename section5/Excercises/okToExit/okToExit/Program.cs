72eec65fa5721777d34ad7934c0fec6ffc35aa96
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-27 20:27:13 -0400

    Add okToExit

---
+++ section5/Excercises/okToExit/okToExit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace okToExit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("enter a number or type \"ok\" to exit: ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                    break;
+                else
+                {
+                    sum += Convert.ToInt32(input);
+
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
