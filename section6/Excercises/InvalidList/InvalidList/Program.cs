7c2da91dbe65e9e3d657d0d319c7a122ee31a51a
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-30 18:33:56 -0400

    Add InvalidList WIP

--- section6/Excercises/InvalidList/InvalidList/Program.cs
+++ section6/Excercises/InvalidList/InvalidList/Program.cs
@@ -6,14 +6,27 @@ namespace InvalidList
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                
             Console.WriteLine("Enter 5 numbers separated by commas: ");
+                var [] numbers = Console.ReadLine();
+                // if less than 5 numbers
+                if (numbers != 5)
+                {
+                    Console.WriteLine("Invalid List! Pease try again.");
+                }
+                else if (var num in numbers == numbers -1)
+                {
+                    Console.WriteLine("Invalid List! Pease try again.");
+                }
+                else 
+                {
+                    numbers.Sort();
+                    // display 3 smallest numbers
+                    Console.WriteLine(numbers[0], numbers[1], numbers[2], );
+                }
 
-            // if less than 5 numbers
-
-            Console.WriteLine("Invalid List! Pease try again.");
-
-            else 
-                // display 3 smallest numbers
         }
     }
 }
