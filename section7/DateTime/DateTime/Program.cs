8e28c32403f02d181a826d5f75a5afe19e2cf693
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-30 21:09:12 -0400

    Add DateTime now

--- section7/DateTime/DateTime/Program.cs
+++ section7/DateTime/DateTime/Program.cs
@@ -12,6 +12,15 @@ namespace DateTime
 
             Console.WriteLine("Hour: "+ now.Hour);
             Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
         }
     }
 }
