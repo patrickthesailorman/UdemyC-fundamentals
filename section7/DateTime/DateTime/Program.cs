7b0b41860b75f03f584e2af872c0993d22071042
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-30 21:01:48 -0400

    Add DateTime

---
+++ section7/DateTime/DateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2018, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: "+ now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+        }
+    }
+}
