2f7f6f2f19b83717122095116bbb35b30b18a5b0
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-07-01 15:53:44 -0400

    Add section8

---
+++ section8/section8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace section8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is section8");
+        }
+    }
+}
