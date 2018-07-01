c4c3f875f22125631255fac8523e5f4b48d43fab
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-06-30 21:19:29 -0400

    Add properties

---
+++ section7/TimeSpan/TimeSpan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timespan = new TimeSpan(1, 2, 3);
+            var timespan1 = new TimeSpan(1, 0, 0);
+            var timespan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+
+            Console.WriteLine("Duration: ", duration);
+
+            // Properties
+            Console.WriteLine("Minutes: ", timespan.Minutes);
+            Console.WriteLine("Total Minutes: ", timespan.TotalMinutes);
+
+        }
+    }
+}
