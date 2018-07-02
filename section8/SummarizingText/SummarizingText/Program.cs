e5ee42672ba0e7679e3850547e4c2504e4b8bd4d
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-07-01 20:35:01 -0400

    Add StringUtility

--- section8/SummarizingText/SummarizingText/Program.cs
+++ section8/SummarizingText/SummarizingText/Program.cs
@@ -8,31 +8,9 @@ namespace SummarizingText
         static void Main(string[] args)
         {
             var sentence = "This is a really really really really really long text.";
-            var summary = SummarizeText(sentence, 25);
+            var summary = StringUtility.SummarizeText(sentence, 25);
             Console.WriteLine(summary);
         }
-        static string SummarizeText(string text, int maxLength = 20)
-        {
-            if (text.Length < maxLength)
-               return text;
-            
 
-            // sentence.Substring(0, maxLength); 
-            var words = text.Split(' ');
-            var totalCharacters = 0;
-            var summaryWords = new List<string>();
-
-            foreach (var word in words)
-            {
-                summaryWords.Add(word);
-
-                totalCharacters += word.Length + 1;
-                if (totalCharacters > maxLength)
-                    break;
-            }
-
-            return String.Join(" ", summaryWords + "...");
-
-        }
     }
 }
