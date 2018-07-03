9d374a748d7b66078b9e058b5da8848ab325701a
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-07-03 15:29:53 -0400

    Add consecutive

--- section8/Excercises/Consecutive/Program.cs
+++ section8/Excercises/Consecutive/Program.cs
@@ -10,8 +10,18 @@ namespace Consecutive
             var input = Console.ReadLine();
             var numbers = input.Split('-');
 
-
             // if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
+            foreach (var number in numbers)
+            {
+                if( number[i] +1 != numbers[i + 1] || number[i] != number[i -1] )
+                Console.WriteLine("Non-Consecutive");
+
+                else
+                {
+                      Console.WriteLine("Consecutive");      
+                }
+
+            }
         }
     }
 }
