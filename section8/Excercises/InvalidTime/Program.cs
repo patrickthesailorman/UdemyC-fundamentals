fa83fa179c601cf346230a40916dbc0230d18dd4
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-07-03 15:39:23 -0400

    Add InvlaidTime

---
+++ section8/Excercises/InvalidTime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace InvalidTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter time: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+            var components = input.Split(':');
+            if (components.Length != 2)
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+            
+            try
+            {
+                var hour = Convert.ToInt32(components[0]);
+                var minute = Convert.ToInt32(components[1]);
+
+                if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+                    Console.WriteLine("Ok");
+                else
+                    Console.WriteLine("Invalid Time");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Time");
+            }
+        }
+    }
+}
