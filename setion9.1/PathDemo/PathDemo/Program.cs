85230edf897249096bb27fb32064dd55d79af370
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-07-05 18:22:49 -0400

    Add pathDemo

---
+++ setion9.1/PathDemo/PathDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PathDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+        }
+    }
+}
