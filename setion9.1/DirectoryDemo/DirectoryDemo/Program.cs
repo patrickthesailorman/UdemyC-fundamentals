4292380d3864bdb91dbbfbf5cdd1822288a6da96
patrickthesailorman <patrickthesaiorman@gmail.com>
2018-07-05 18:18:19 -0400

    Add directories path

--- setion9.1/DirectoryDemo/DirectoryDemo/Program.cs
+++ setion9.1/DirectoryDemo/DirectoryDemo/Program.cs
@@ -9,9 +9,19 @@ namespace DirectoryDemo
         {
             Directory.CreateDirectory(@"c:\temp\folder1");
 
-            var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
-            foreach (var file in files)
-            Console.WriteLine(file);
+            //var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.sln", SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
         }
     }
 }
